b068b104894d73d5a7a989917dc9cf21a716942a
Ganeshk750 <ganesh9ot@gmail.com>
2019-11-14 21:02:03 +0530

    connecting to databse

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const Port = process.env.PORT || 3000;
+const path = require('path');
+const myConnection = require('express-myconnection');
+const mysql = require('mysql');
 
+
+//Setting ups
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+//connecting to database
+app.use(myConnection(mysql,{
+  host: 'localhost',
+  user: 'root',
+  password: 'ganesh',
+  port: 3306,
+  database: 'nodewithmysql'
+}, 'single'));
 
 //For testing
 app.get('/',(req,res) =>{
     res.send('Wel come to Nodejs world');
 })
 
-app.listen(port, () => {
-    console.log(`Server is running on this ${port}`)
+app.listen(Port, () => {
+    console.log(`Server is running on this ${Port}`)
 });
 
 
